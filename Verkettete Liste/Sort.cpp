953184f9eb1f093065e1ac48adc46bdb2d3e21c8
Kithrian <pallene@protonmail.com>
2019-12-20 15:16:28 +0100

    asfa

---
+++ Verkettete Liste/Sort.cpp
@@ -0,0 +1,20 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+void bubblesort(int *array, int length)
+{
+  int i, j, tmp;
+
+  for (i = 1; i < length; i++)
+  {
+    for (j = 0; j < length - i; j++)
+    {
+      if (array[j] > array[j + 1])
+      {
+        tmp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = tmp;
+      }
+    }
+  }
+}
